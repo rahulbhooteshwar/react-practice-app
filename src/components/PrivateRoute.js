56571a09dd39bb629fe7b589aadb1f59f8abe025
Rahul Bhooteshwar <rahul.bhooteshwar@gmail.com>
2020-07-29 09:26:58 +0530

    add redirectTo feature

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import { Route, Redirect } from 'react-router'
+import { Route, Redirect, useLocation } from 'react-router'
 import { useContext } from 'react'
 import { AuthContext } from '../context/AuthContext'
 
 const PrivateRoute = ({ children, ...rest }) => {
+  const { pathname } = useLocation();
   const { authStatus } = useContext(AuthContext);
   return (
-    authStatus ? <Route {...rest}>{children}</Route> : <Redirect to='/auth'/>
+    authStatus
+      ? <Route {...rest}>{children}</Route>
+      : <Redirect to={`/auth?redirectTo=${pathname}`}
+    />
   )
 }
 
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { useState } from 'react';
-import { useHistory } from 'react-router';
+import { useHistory, useLocation } from 'react-router';
 export const AuthContext = React.createContext({ status: false, login: () => { }, logout: () => { } });
 
 export const AuthProvider = ({ children }) => {
   const [authStatus, setAuthStatus] = useState(false);
   const history = useHistory()
+  const { search } = useLocation();
+  const query = new URLSearchParams(search);
   const login = () => {
     setAuthStatus(true)
-    history.push('/products')
+    if (query.get('redirectTo')) {
+      history.push(query.get('redirectTo'))
+    } else {
+      history.push('/products')
+    }
   }
   const logout = () => {
     setAuthStatus(false)
